b58034c6d9b0c8857f79787359c712ae006d9180
Aniket Saini <aniketsaini252000@gmail.com>
2024-11-18 20:06:54 +0530

    setup the project with all files, level 0

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAOaGBXO64yt4x7uhXxN3ltNuS4F-3xy78",
+    authDomain: "netflix-clone-63e56.firebaseapp.com",
+    projectId: "netflix-clone-63e56",
+    storageBucket: "netflix-clone-63e56.firebasestorage.app",
+    messagingSenderId: "186134932290",
+    appId: "1:186134932290:web:dd6d5225b5cf87e34076db"
+  };
