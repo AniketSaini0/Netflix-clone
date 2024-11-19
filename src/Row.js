c9218700fd3ab21ca69debd8a6aa257b2c715ab2
Aniket Saini <aniketsaini252000@gmail.com>
2024-11-20 02:08:17 +0530

    fix the render size and apply condition for avoiding broken poster(links)

--- src/Row.js
+++ src/Row.js
@@ -27,11 +27,16 @@ function Row({ title, fetchUrl, isLargeRow = false }) {
             <div className="row__posters">
                 {/* we need to iterate over all the movies , ifLargeRow is given then we need to use movie.poster_path */}
                 {movies.map((movie) => (
-                    <img className={`row__poster ${isLargeRow && "row__poasterLarger"}`}
-                        key={movie.id}
-                        src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name} />
-                    ))
-                }
+                    //This condition is to avoid breaks(means render only those movie posters which are available according tot he conditions.)
+                    ((isLargeRow && movie.poster_path) || (!isLargeRow && movie.backdrop_path)) &&
+                    (
+                        <img className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                            key={movie.id}
+                            src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+                            alt={movie.name} />
+                    )
+                    ))}
+                    
             </div>
 
         </div>
