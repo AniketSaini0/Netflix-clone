2fc5dad175e37d4248e43cdcdc1581441fb72af8
Aniket Saini <aniketsaini252000@gmail.com>
2024-11-19 15:12:17 +0530

    create navbar with transiton

---
+++ src/HomeScreen.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import "./HomeScreen.css";
+import Navbar from './Navbar';
+
+function HomeScreen() {
+  return (
+    <div className='homeScreen'>
+       <Navbar/>
+       <h1>test</h1>
+       <h1>test</h1>
+       <h1>test</h1>
+       <h1>test</h1>
+       <h1>test</h1>
+       <h1>test</h1>
+       <h1>test</h1>
+       <h1>test</h1>
+       <h1>test</h1>
+       <h1>test</h1>
+       <h1>test</h1>
+       <h1>test</h1>
+       <h1>test</h1>
+       <h1>test</h1>
+       <h1>test</h1>
+       <h1>test</h1>
+       <h1>test</h1>
+       <h1>test</h1>
+       <h1>test</h1>
+       <h1>test</h1>
+       <h1>test</h1>
+       <h1>test</h1>
+       <h1>test</h1>
+       <h1>test</h1>
+       <h1>test</h1>
+       <h1>test</h1>
+       <h1>test</h1>
+       <h1>test</h1>
+       <h1>test</h1>
+       <h1>test</h1>
+       <h1>test</h1>
+       <h1>test</h1>
+       <h1>test</h1>
+       <h1>test</h1>
+       <h1>test</h1>
+       <h1>test</h1>
+       <h1>test</h1>
+    </div>
+  )
+}
+
+export default HomeScreen
