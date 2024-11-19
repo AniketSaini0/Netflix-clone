4b89dbe3236ddf3f65d87c16af49937f2c4d846e
Aniket Saini <aniketsaini252000@gmail.com>
2024-11-20 01:09:49 +0530

    create rows and fetch all categories movies to populate rows

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -2,6 +2,9 @@ import React from 'react';
 import "./HomeScreen.css";
 import Navbar from './Navbar';
 import Banner from './Banner';
+import requests from './Requests';
+import Row from './Row';
+
 
 function HomeScreen() {
     return (
@@ -10,22 +13,15 @@ function HomeScreen() {
 
             <Banner />
 
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-
+             <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} isLargeRow/>
+             <Row title="Trending Now" fetchUrl={requests.fetchTopRated}/>
+             <Row title="Top Rated" fetchUrl={requests.fetchTrending}/>
+             <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
+             <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+             <Row title="Horror MOvies" fetchUrl={requests.fetchHorrorMovies}/>
+             <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
+             <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}/>
+             
         </div>
     )
 }
---
+++ src/Row.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import "./Row.css";
+import "./Requests";
+import requests from './Requests';
+import axios from "./axios";
+
+function Row({ title, fetchUrl, isLargeRow = false }) {
+    const [movies, setMovies] = useState([]);
+    const base_url = "https://image.tmdb.org/t/p/original/";
+
+    //this is for fetching all the movies under that category
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(fetchUrl);
+            setMovies(request.data.results);
+
+            return request;
+        }
+        fetchData();
+    }, []);
+    //here we have all the movies
+    console.log(movies);
+
+    return (
+        <div className='row'>
+            <h2>{title}</h2>
+            <div className="row__posters">
+                {/* we need to iterate over all the movies , ifLargeRow is given then we need to use movie.poster_path */}
+                {movies.map((movie) => (
+                    <img className={`row__poster ${isLargeRow && "row__poasterLarger"}`}
+                        key={movie.id}
+                        src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name} />
+                    ))
+                }
+            </div>
+
+        </div>
+    )
+}
+
+export default Row
